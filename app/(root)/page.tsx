21ddc2d74c0fa26570fe50d50d92cd257225fd94
Dhrish Parekh <parekhdhrish.pg@gmail.com>
2023-09-30 23:21:58 +0530

    updated folder structure

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,12 @@
 import Image from 'next/image'
 import Landing from '@/components/landing'
 import { Navbarmain } from '@/components/navbarmain'
+import { Navbar } from '@/components/navbar'
 export default function Home() {
   return (
    <>
    <div className="background_gr">
-   <Navbarmain/>
+   <Navbar/>
    <Landing/>
    </div>/
    </>
